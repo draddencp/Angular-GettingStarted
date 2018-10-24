e0822b71fd30c4e654897da49837a298adf2845e
draddencp <daniel.radden@crownpeak.com>
2018-10-24 16:05:14 -0600

    updated

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product/product-list.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { FormsModule } from '@angular/forms';
+import { StarComponent } from './shared/star.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductDetailComponent } from './product/product-detail.component';
+import {WelcomeComponent} from './home/welcome.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConvertToSpacesPipe,
+    StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent},
+      { path: 'products/:id', component: ProductDetailComponent},
+      { path: 'welcome', component: WelcomeComponent},
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+
+    ])
   ],
   bootstrap: [AppComponent]
 })
